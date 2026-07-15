cef5a892ec847aa9dfbed0e8b384dc6896771dab
modestmind <modestmind@naver.com>
2026-07-15 19:27:36 +0900

    main fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router.tsx";
 import { Provider } from "react-redux";
 import store from "./store/store.ts";
-import { setupWorker } from "msw/browser";
-import handlers from "./server/msw/handlers.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
